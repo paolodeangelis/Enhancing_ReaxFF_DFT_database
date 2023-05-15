c8e755e5f010abc04469f17d31cdaeee500d6e10
paolodeangelis <paolo.deangelis@polito.it>
2023-05-15 12:32:38 +0200

    ⚒️ add docstring

---
+++ tools/plasm_experimental/__init__.py
@@ -0,0 +1,14 @@
+"""This module provides experimental functionalities for the PLAMS package.
+
+It includes additional calculators, utilities, and enhancements that are still under development or testing.
+
+The experimental functionalities are:
+- ASE_Calculator: A calculator for the ASE package.
+
+Module Name: plasm_experimental
+Authors: Paolo De Angelis (paolo.deangelis@polito.it)
+Copyright (c) 2023 Paolo De Angelis
+"""
+from .ase_calculator import AMSCalculator
+
+__all__ = ["AMSCalculator"]
